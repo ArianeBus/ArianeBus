7818ea051e10b6d28131e5b4b5014b8c267346d7
Marc Chouteau <marc_chouteau@hotmail.com>
2024-02-10 17:42:41 +0100

    Fix SerializedMessage

--- src/ArianeBus.MediatR/MediatRExtensions.cs
+++ src/ArianeBus.MediatR/MediatRExtensions.cs
@@ -4,7 +4,8 @@ namespace MediatR;
 
 public static class MediatRExtensions
 {
-	public static async Task GlobalPublish(this IMediator mediator, INotification notification, CancellationToken cancellationToken = default)
+	public static async Task GlobalPublish<T>(this IMediator mediator, T notification, CancellationToken cancellationToken = default)
+		where T : INotification
 	{
 		if (notification is null)
 		{
@@ -13,9 +14,10 @@ public static class MediatRExtensions
 		var busConfig = await mediator.Send(new GetBusRequest(), cancellationToken);
 		var aqn = notification.GetType().AssemblyQualifiedName!.Split(',');
 		var simplifiedAqn = $"{aqn[0]},{aqn[1]}";
+		string notifString = System.Text.Json.JsonSerializer.Serialize(notification, ArianeBus.JsonSerializer.Options);
 		var message = new NotificationMessage
 		{
-			SerializedNotification = System.Text.Json.JsonSerializer.Serialize(notification, ArianeBus.JsonSerializer.Options),
+			SerializedNotification = notifString,
 			NotificationFullTypeName = simplifiedAqn
 		};
 		await busConfig.Bus.PublishTopic(busConfig.Configuration.TopicName, message, cancellationToken: cancellationToken);
